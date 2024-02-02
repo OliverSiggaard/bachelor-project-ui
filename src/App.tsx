9e7e502b7afc429fa288c31a7fff4aeaf58566da
Oliver Siggaard <s204450@student.dtu.dk>
2024-02-02 15:43:21 +0100

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig';
 
 function App() {
+
+  const [helloString, setHelloString] = useState<string>('');
+
+  const getHelloString = async () => {
+    const response = await api.get("/root");
+    setHelloString(response.data);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={getHelloString}>
+          Press Me
+        </button>
+        {helloString}
       </header>
     </div>
   );
